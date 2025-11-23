1562efb9874174641d72c96ac3cc4b8dd50a06f7
TheNotary <no@email.plz>
2025-11-23 11:55:47 -0600

    fleshes out most of the layout

--- src/tui.rs
+++ src/tui.rs
@@ -22,17 +22,27 @@ pub fn tui_loop(app: &App) -> Result<()> {
 }
 
 fn draw(frame: &mut Frame, app: &App) {
+    let mut controls_lines = vec![];
+    let controls_txts = [
+        "Use j/k to scroll the lore. Use e to enter edit mode to define your sql.",
+        "Use t to enter talk mode. Use [enter] to test your solution.",
+    ];
+    for controls_txt in controls_txts {
+        controls_lines.push(Line::from(controls_txt));
+    }
+
     use Constraint::{Fill, Length, Min};
 
-    let vertical = Layout::vertical([Length(3), Min(0), Length(3)]);
-    let [title_area, main_area, status_area] = vertical.areas(frame.area());
+    let vertical = Layout::vertical([Length(3), Min(0), Length(4), Length(3)]);
+    let [title_area, main_area, controls_area, input_area] = vertical.areas(frame.area());
     let horizontal = Layout::horizontal([Fill(1); 2]);
     let [lore_area, output_area] = horizontal.areas(main_area);
 
     let title_block = Block::bordered().title(Line::from("STAGE").centered());
     let output_block = Block::bordered().title("CPU");
-    let input_block = Block::bordered().title(Line::from("INPUT").centered());
     let lore_block = Block::bordered().title("LORE");
+    let controls_block = Block::bordered().title(Line::from("").centered());
+    let input_block = Block::bordered().title(Line::from("INPUT").centered());
 
     let lore_text = Paragraph::new(app.lore.clone())
         .block(lore_block)
@@ -44,10 +54,15 @@ fn draw(frame: &mut Frame, app: &App) {
     let output_text = Paragraph::new(app.output.clone())
         .block(output_block)
         .wrap(Wrap { trim: true });
+    let controls_text = Paragraph::new(controls_lines)
+        .block(controls_block)
+        .centered()
+        .wrap(Wrap { trim: true });
 
     frame.render_widget(title_text, title_area);
     frame.render_widget(output_text, output_area);
     frame.render_widget(lore_text, lore_area);
+    frame.render_widget(controls_text, controls_area);
 
-    frame.render_widget(input_block, status_area);
+    frame.render_widget(input_block, input_area);
 }
