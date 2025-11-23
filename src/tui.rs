8be3889562f022f91f0bca00b3f16d0299cf1944
TheNotary <no@email.plz>
2025-11-23 09:44:27 -0600

    learning layouts

--- src/tui.rs
+++ src/tui.rs
@@ -1,5 +1,12 @@
 use crossterm::event::{self, Event};
-use ratatui::{Frame, text::Text};
+
+use ratatui::{
+    Frame,
+    layout::{Constraint, Layout},
+    style::Style,
+    text::{Line, Text},
+    widgets::{Block, Paragraph},
+};
 
 use crate::Result;
 
@@ -15,7 +22,30 @@ pub fn tui_loop(resp: &str) -> Result<()> {
     Ok(())
 }
 
-fn draw(frame: &mut Frame, msg: &str) {
-    let text = Text::raw(msg);
-    frame.render_widget(text, frame.area());
+fn draw(frame: &mut Frame, lore_str: &str) {
+    let level_name = "lvl 1 - Strongest Cubical";
+
+    use Constraint::{Fill, Length, Min};
+
+    let vertical = Layout::vertical([Length(3), Min(0), Length(3)]);
+    let [title_area, main_area, status_area] = vertical.areas(frame.area());
+    let horizontal = Layout::horizontal([Fill(1); 2]);
+    let [lore_area, output_area] = horizontal.areas(main_area);
+
+    let title_block = Block::bordered().title(Line::from("Title Bar").centered());
+    let output_block = Block::bordered().title("CPU");
+    let status_bar_block = Block::bordered().title("Status Bar");
+    let lore_block = Block::bordered().title("Lore");
+
+    let title_text = Paragraph::new(level_name).block(title_block).centered();
+    let output_text = Paragraph::new(lore_str).block(output_block);
+
+    frame.render_widget(title_text, title_area);
+    frame.render_widget(output_text, output_area);
+
+    frame.render_widget(status_bar_block, status_area);
+    frame.render_widget(lore_block, lore_area);
+
+    // frame.render_widget(title_block, title_area);
+    // frame.render_widget(output_block, output_area);
 }
